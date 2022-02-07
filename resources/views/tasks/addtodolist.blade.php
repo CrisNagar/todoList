9cae873e02bdf3a0178b9582dcc7b4af80979100
CrisNagar <crisnagardev@gmail.com>
2022-02-07 23:24:50 +0100

    design form

--- resources/views/tasks/addtodolist.blade.php
+++ resources/views/tasks/addtodolist.blade.php
@@ -15,13 +15,13 @@
                     <div class="container">
                         <div class="vstack gap-3">
                             <div class="form-floating">
-                                <input type="email" class="form-control" id="taskListTitle" name="taskItem[title]">
+                                <input type="email" class="form-control" id="taskListTitle" name="title">
                                 <label for="taskListTitle">{{ __('Title:') }}</label>
                             </div>
 
                             <div class="form-floating mb-3">
                                 <textarea class="form-control" id="taskListDescription" rows="25"
-                                    name="taskItem[description]"></textarea>
+                                    name="description"></textarea>
                                 <label for="taskListDescription">{{ __('Description:') }}</label>
                             </div>
 
@@ -49,15 +49,16 @@
 
 
                                         <div class="btn-group" role="group">
-                                            <input type="checkbox" class="btn-check" name="taskItem[0][php]" id="php0">
+                                            <input type="checkbox" class="btn-check" name="taskItem[0][php]"
+                                                id="php0">
                                             <label class="btn btn-outline-info text-dark" for="php0">PHP</label>
 
-                                            <input type="checkbox" class="btn-check" id="javascript0"
-                                                name="taskItem[0][javascript]">
-                                            <label class="btn btn-outline-info text-dark"
-                                                for="javascript0">JavaScript</label>
+                                            <input type="checkbox" class="btn-check" id="js0"
+                                                name="taskItem[0][js]">
+                                            <label class="btn btn-outline-info text-dark" for="js0">JavaScript</label>
 
-                                            <input type="checkbox" class="btn-check" name="taskItem[0][css]" id="css0">
+                                            <input type="checkbox" class="btn-check" name="taskItem[0][css]"
+                                                id="css0">
                                             <label class="btn btn-outline-info text-dark" for="css0">CSS</label>
                                         </div>
                                     </div>
@@ -69,8 +70,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-outline-danger"
-                    data-bs-dismiss="modal">{{ __('Close') }}</button>
+                <button type="reset" class="btn btn-outline-danger">{{ __('Clean') }}</button>
                 <button type="button" class="btn btn-outline-success" id="saveTaskBtn">{{ __('Save') }}</button>
             </div>
         </div>
