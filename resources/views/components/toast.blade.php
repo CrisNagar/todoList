87adf057620e9d3d3f1c59c832214972ed1b34a0
CrisNagar <crisnagardev@gmail.com>
2022-02-09 20:21:36 +0100

    add toast component

---
+++ resources/views/components/toast.blade.php
@@ -0,0 +1,8 @@
+<div id="toastAlert" class="toast align-items-center text-white border-0" role="alert" aria-live="assertive"
+    aria-atomic="true" style="width: 18rem;">
+    <div class="d-flex">
+        <div id="toastMessage" class="toast-body"></div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toastAlert"
+            aria-label="Close"></button>
+    </div>
+</div>
