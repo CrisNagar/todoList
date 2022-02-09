7df3c95f146d0688b2fbac1f2ca960fba559c902
CrisNagar <crisnagardev@gmail.com>
2022-02-09 20:24:34 +0100

    refactor
    add toast logic
    refactor ajax methods
    refactor insert new taskitem
    add actions buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,7 +94,8 @@
     <script>
         document.addEventListener('DOMContentLoaded', load, false);
 
-        const modal = new bootstrap.Modal(document.getElementById('addTaskModal'));
+        const modal = new bootstrap.Modal(document.getElementById('taskFormModal'));
+        const toast = new bootstrap.Toast(document.getElementById('toastAlert'));
 
         function load() {
             $.ajaxSetup({
@@ -108,15 +109,26 @@
             });
 
             btnListener('modals');
+            setActionButtons();
+            initTooltips();
 
+        }
+
+        function toggleModal() {
+            modal.toggle();
+        }
+
+        function initTooltips() {
             var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
             var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
                 return new bootstrap.Tooltip(tooltipTriggerEl)
             })
         }
 
-        function showModal() {
-            modal.toggle();
+        function toggleToast(type, message) {
+            document.getElementById('toastAlert').setAttribute('class', `bg-${type} position-absolute bottom-0 end-0 m-3`);
+            document.getElementById('toastMessage').innerText = message;
+            toast.show()
         }
 
         function addNewTaskItem() {
@@ -147,7 +159,7 @@
             let inputText = document.createElement('input');
             inputText.setAttribute('class', 'form-control');
             inputText.setAttribute('type', 'text');
-            inputText.setAttribute('name', 'taskItem[' + number + '][title]');
+            inputText.setAttribute('name', `taskItem[${number}][title]`);
 
             let label = document.createElement('label');
             label.innerText = 'Task:';
@@ -159,34 +171,34 @@
             let btnPHP = document.createElement('input');
             btnPHP.setAttribute('class', 'btn-check');
             btnPHP.setAttribute('type', 'checkbox');
-            btnPHP.setAttribute('id', 'php' + number + '');
-            btnPHP.setAttribute('name', 'taskItem[' + number + '][php]');
+            btnPHP.setAttribute('id', `php${number}`);
+            btnPHP.setAttribute('name', `taskItem[${number}][php]`);
 
             let labelPHP = document.createElement('label');
             labelPHP.setAttribute('class', 'btn btn-outline-info text-dark');
-            labelPHP.setAttribute('for', 'php' + number + '');
+            labelPHP.setAttribute('for', `php${number}`);
             labelPHP.innerText = 'PHP';
 
             let btnJS = document.createElement('input');
             btnJS.setAttribute('class', 'btn-check');
             btnJS.setAttribute('type', 'checkbox');
-            btnJS.setAttribute('id', 'js' + number + '');
-            btnJS.setAttribute('name', 'taskItem[' + number + '][js]');
+            btnJS.setAttribute('id', `js${number}`);
+            btnJS.setAttribute('name', `taskItem[${number}][js]`);
 
             let labelJS = document.createElement('label');
             labelJS.setAttribute('class', 'btn btn-outline-info text-dark');
-            labelJS.setAttribute('for', 'js' + number + '');
+            labelJS.setAttribute('for', `js${number}`);
             labelJS.innerText = 'JavaScript';
 
             let btnCSS = document.createElement('input');
             btnCSS.setAttribute('class', 'btn-check');
             btnCSS.setAttribute('type', 'checkbox');
-            btnCSS.setAttribute('id', 'css' + number + '');
-            btnCSS.setAttribute('name', 'taskItem[' + number + '][css]');
+            btnCSS.setAttribute('id', `css${number}`);
+            btnCSS.setAttribute('name', `taskItem[${number}][css]`);
 
             let labelCSS = document.createElement('label');
             labelCSS.setAttribute('class', 'btn btn-outline-info text-dark');
-            labelCSS.setAttribute('for', 'css' + number + '');
+            labelCSS.setAttribute('for', `css${number}`);
             labelCSS.innerText = 'CSS';
 
             btnGroup.appendChild(btnPHP);
@@ -223,7 +235,8 @@
                 let modalButtons = document.getElementsByClassName('btn-show-modal');
                 for (let i = 0; i < modalButtons.length; i++) {
                     modalButtons[i].addEventListener('click', function() {
-                        showModal();
+                        ACTION_FORM = 'create';
+                        toggleModal();
                         btnListener('newTasks');
                         btnListener('deleteNewTaskItem');
                     });
@@ -255,18 +268,73 @@
             }
         }
 
+        function setActionButtons() {
+            const btnDelete = document.getElementsByClassName('btn-delete');
+            const btnEdit = document.getElementsByClassName('btn-edit');
+            const btnResolve = document.getElementsByClassName('btn-resolve');
+
+            for (let i = 0; i < btnDelete.length; i++) {
+                btnDelete[i].addEventListener('click', function() {
+                    deleteTask(this.dataset.task);
+                });
+            }
+
+            for (let i = 0; i < btnEdit.length; i++) {
+                btnEdit[i].addEventListener('click', function() {
+                    editTask(this.dataset.task);
+                });
+            }
+
+            for (let i = 0; i < btnResolve.length; i++) {
+                btnResolve[i].addEventListener('click', function() {
+                    resolveTask(this.dataset.task);
+                });
+            }
+        }
+
         function saveTask(task) {
             let formData = new FormData(task);
             formData.append('_token', '{{ csrf_token() }}');
 
             $.ajax({
-                url: "{{ route('todolist_store') }}",
+                url: "{{ route('task.store') }}",
                 method: 'POST',
-                data: formData,
+                data: formData
+            }).done((res) => {
+                task.reset();
+                toggleModal();
+                document.getElementById('taskTable').innerHTML = res;
+            }).fail((error) => {
+                toggleModal();
+                toggleToast('danger', 'Save task is FAIL!');
+                console.log(error);
+            });
+        }
+
+        function editTask(task) {
+            let url = "{{ route('task.edit', ':id') }}";
+            url = url.replace(':id', task);
+            $.ajax({
+                url: url,
+                method: 'POST',
+            }).done((res) => {
+                task.reset();
+                toggleModal();
+                console.log(res);
+            }).fail((error) => {
+                console.log(error);
+            });
+        }
+
+        function deleteTask(task) {
+            let url = "{{ route('task.destroy', ':id') }}";
+            url = url.replace(':id', task);
+
+            $.ajax({
+                url: url,
+                method: 'DELETE',
                 success: function(res) {
-                    showModal();
                     reloadData();
-                    task.reset();
                     console.log(res);
                 },
                 error: function(error) {
@@ -275,24 +343,40 @@
             });
         }
 
-        function editTask(task) {}
-
-        function deleteTask(task) {}
-
-        function resolveTask(task) {}
+        function resolveTask(task) {
+            $.ajax({
+                url: "{{ route('task.store') }}",
+                method: 'POST',
+                data: task,
+                success: function(res) {
+                    toggleModal();
+                    reloadData();
+                    task.reset();
+                    console.log(res.message);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        }
 
         function reloadData() {
+            let url = "{{ route('task.index', ['reloadData' => ':needReload']) }}";
+            url = url.replace(':needReload', true);
+
             $.ajax({
-                url: "{{ route('todolist_index') }}",
+                url: url,
                 method: 'GET',
-                data: "reloadData='true'",
                 success: function(res) {
+                    setActionButtons();
                     console.log(res);
                 },
                 error: function(error) {
-                    console.log(error.message);
+                    console.log(error);
                 }
             });
+
+            location.reload();
         }
     </script>
     @yield('script')
