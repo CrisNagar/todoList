06e6a953eec5004465593dc4db380102884abab8
CrisNagar <crisnagardev@gmail.com>
2022-02-10 17:53:26 +0100

    toast

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,4 +1,4 @@
-<div id="toastAlert" class="toast align-items-center text-white border-0" role="alert" aria-live="assertive"
+<div id="toastAlert" class="toast align-items-center text-white border-0 show" role="alert" aria-live="assertive"
     aria-atomic="true" style="width: 18rem;">
     <div class="d-flex">
         <div id="toastMessage" class="toast-body"></div>
